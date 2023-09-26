fb46b2b86a0b0c07e55a45e2db863457710da4b8
Kwonsangelic <124767182+Kwonsangelic@users.noreply.github.com>
2023-09-26 18:15:47 +0700

    tse
    fre

---
+++ php/Home_Page/portofoliopengajar.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+
+
+<head>
+    
+    <title>portofolio pengajar</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="category.css">
+  
+</head>
+
+
+<body>
+
+<div class="container mt-3">
+
+<div class="row">
+<h2>Portofolio Pengajar</h2>
+
+<div class="col-3" style="margin:10px;">
+                <div class="card" style="width:300px">
+                    <div class="card-body">
+                        <h4 class="card-title">Nama Pengajar </h4>
+                        <h6>Mengajar Pelajaran Apa</h6>
+                        <img class="card-img-top" src="foto_pengajar/guru1.jpg" alt="Card image" style="width:100%;border-radius: 50px;">
+                        <hr>
+                        <p class="card-text" onmouseover="showPopup(this)" onmouseout="hidePopup(this)">Deskripsi lengkap, riwayat pendidikan, sosmed, dll </p>
+                        <a href="#" class="btn btn-primary">See Profile</a>
+                    </div>
+                </div>
+            </div>
+
+
+</div>
+    
+</body>
+
+
+</html>
