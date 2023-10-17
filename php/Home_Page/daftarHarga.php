eff2d895c1fa2f8ef5d437e24a1b12fc295b635f
agathacelia <90375290+agathacelia@users.noreply.github.com>
2023-10-17 15:54:02 +0700

    Daftar Harga Kursus

--- php/Home_Page/daftarHarga.php
+++ php/Home_Page/daftarHarga.php
@@ -1,43 +1,54 @@
 <!DOCTYPE html>
-<html lang = "en">
-
+<html lang="en">
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Daftar Harga Kursus</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+</head>
+<body>
+    <div class="container">
+        <h1>Daftar Harga Kursus Bahasa Inggris</h1>
+        <div class="row">
+            <?php
+            
+            $host = "localhost"; 
+            $username = "username";
+            $password = "password"; 
+            $database = "hargakursus"; 
+            
+            
+            $conn = mysqli_connect($host, $username, $password, $database);
+            
+            
+            if (!$conn) {
+                die("Koneksi ke database gagal: " . mysqli_connect_error());
+            }
 
-<title>Daftar Harga Kursus</title>
-<meta charset="UTF-8".
-<meta name="viewport" content="width=device-width, inntial-scale=1.0">
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-<script src="https://cdn.jsdelivr.net/npm/boostrap@5.3.0/dist/js/bootstrap.bundle.min.js></script>
+            
+            $sql = "SELECT * FROM hargakursus";
+            $result = mysqli_query($conn, $sql);
 
-<?php
-$daftarHarga = [
-    ["Beginner", "8 kali pertemuan", "Rpxxx"],
-    ["Intermediate", "12 kali pertemuan", "Rpxxx"],
-    ["Expert", "16 kali pertemuan", "Rpxxx"]
-];
-?>
-
-<!-- Tabel Daftar Harga Kursus -->
-<table>
-    <thead>
-        <tr>
-            <th>Level</th>
-            <th>Jumlah Pertemuan</th>
-            <th>Harga</th>
-        </tr>
-    </thead>
-    <tbody>
-        <?php
-        foreach ($daftarHarga as $kursus) {
-            echo "<tr>";
-            echo "<td>{$kursus[0]}</td>";
-            echo "<td>{$kursus[1]}</td>";
-            echo "<td>{$kursus[2]}</td>";
-            echo "</tr>";
-        }
-        ?>
-    </tbody>
-</table>
-
-    </body>
-    </html>
+            if (mysqli_num_rows($result) > 0) {
+                while ($row = mysqli_fetch_assoc($result)) {
+            ?>
+                <div class="col-lg-4 col-md-6 mb-4">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title"><?php echo $row['id_kelas']; ?></h5>
+                            <p class="card-text">Sesi per Bulan: <?php echo $row['sesi_perBulan']; ?></p>
+                            <p class="card-text">Harga: <?php echo $row['harga']; ?></p>
+                        </div>
+                    </div>
+                </div>
+            <?php
+                }
+            } else {
+                echo "Belum ada data harga kursus yang tersedia.";
+            }
+            ?>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
