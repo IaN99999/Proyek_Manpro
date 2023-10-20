4d6f20aa93ff13ba1f9e7a4bd4f1c1cfedbf2a13
Kwonsangelic <124767182+Kwonsangelic@users.noreply.github.com>
2023-10-20 20:10:11 +0700

    dfgh

--- php/Home_Page/portofoliopengajar.php
+++ php/Home_Page/portofoliopengajar.php
@@ -1,19 +1,68 @@
 <?php include '../navbar_footer/navbar.php'; ?>
 
 <div class="container mt-3" style="padding-top: 5%;">
-    <div class="row">
+    <div class="row" style="padding-top:2%">
         <h2>Portofolio Pengajar</h2>
-        <div class="col-3" style="margin:10px;">
-            <div class="card" style="width:300px">
+        <div class="col-12" style="padding-top:3%">
+
+
+            <div class="card" >
                 <div class="card-body">
-                    <h4 class="card-title">Nama Pengajar </h4>
-                    <h6>Mengajar Pelajaran Apa</h6>
-                    <img class="card-img-top" src="foto_pengajar/guru1.jpg" alt="Card image" style="width:100%;border-radius: 50px;">
-                    <hr>
-                    <p class="card-text" onmouseover="showPopup(this)" onmouseout="hidePopup(this)">Deskripsi lengkap, riwayat pendidikan, sosmed, dll </p>
-                    <a href="#" class="btn btn-primary">See Profile</a>
+                    <div class="row">
+                        <div class="col-2" style="border-right: solid 1px;">
+                        <img class="card-img-top" src="../../assets/foto_pengajar/guru1.jpg" alt="Card image" style="width:100%;border-radius: 50px;">
+                        </div>
+                        <div class="col-8">
+                            <h4 class="card-title">Nama Pengajar </h4>
+                            <h6>Mengajar Pelajaran Apa</h6>
+                            <p class="card-text" onmouseover="showPopup(this)" onmouseout="hidePopup(this)">Deskripsi lengkap, riwayat pendidikan, sosmed, dll </p>
+                            <a href="#" class="btn btn-primary">See Profile</a>
+                        </div>
+                    </div>
                 </div>
             </div>
+            
+            <br>
+
+            <div class="card" >
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-2" style="border-right: solid 1px;">
+                        <img class="card-img-top" src="../../assets/foto_pengajar/guru1.jpg" alt="Card image" style="width:100%;border-radius: 50px;">
+                        </div>
+                        <div class="col-8">
+                            <h4 class="card-title">Nama Pengajar </h4>
+                            <h6>Mengajar Pelajaran Apa</h6>
+                            <p class="card-text" onmouseover="showPopup(this)" onmouseout="hidePopup(this)">Deskripsi lengkap, riwayat pendidikan, sosmed, dll </p>
+                            <a href="#" class="btn btn-primary">See Profile</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <br>
+
+            <div class="card" >
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-2" style="border-right: solid 1px;">
+                        <img class="card-img-top" src="../../assets/foto_pengajar/guru1.jpg" alt="Card image" style="width:100%;border-radius: 50px;">
+                        </div>
+                        <div class="col-8">
+                            <h4 class="card-title">Nama Pengajar </h4>
+                            <h6>Mengajar Pelajaran Apa</h6>
+                            <p class="card-text" onmouseover="showPopup(this)" onmouseout="hidePopup(this)">Deskripsi lengkap, riwayat pendidikan, sosmed, dll </p>
+                            <a href="#" class="btn btn-primary">See Profile</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+
+
+
+
         </div>
         <div class="col-9">
 
---
+++ php/Login_Logout/login.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html> 
+<html lang="en"> 
+<head> 
+    <meta charset="UTF-8"> 
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+    <title>Full Screen Two Column Page with Background Image (Bootstrap)</title> 
+    <!-- Include Bootstrap CSS --> 
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" rel="stylesheet"> 
+    <style> 
+        body, html { 
+            height: 100%; 
+            margin: 0; 
+        } 
+        .column1 { 
+            background-image: url('../../assets/asset_web/bg.png'); /* Replace with the actual path to your image */ 
+            background-size: cover; 
+            background-position: center; 
+            color: white; /* Set text color for better readability */ 
+            height: 100%; 
+        } 
+    </style> 
+</head> 
+<body> 
+    <div class="container-fluid h-100"> 
+        <div class="row h-100"> 
+            <!-- Column 1 with background image --> 
+            <div class="col-md-6 column1"> 
+                <!-- Content for the first column goes here --> 
+            </div> 
+ 
+            <!-- Column 2 --> 
+            <div class="col-md-6"> 
+                <!-- Content for the second column goes here --> 
+            </div> 
+        </div> 
+    </div> 
+ 
+    <!-- Include Bootstrap JS (Optional) --> 
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script> 
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js"></script> 
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script> 
+</body> 
+</html>
