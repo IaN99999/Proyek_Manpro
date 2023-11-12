fe17c45df77eec6642fd36a98c4af3696e81a326
se7entyx <christopherafael77@gmail.com>
2023-11-13 00:24:30 +0700

    fix tampilan penilaian

--- php/Guru_Page/daftarSiswa.php
+++ php/Guru_Page/daftarSiswa.php
@@ -11,59 +11,86 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
     <title>Document</title>
 
     <style>
-        body {
-            margin: 0;
-            font-family: 'Montserrat', sans-serif;
-        }
-        ul {
-            list-style-type: none;
+        @import url("https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap");
+        *{
             margin: 0;
             padding: 0;
-            width: 20%;
-            background-color: #f1f1f1;
-            position: fixed;
-            height: 100%;
-            overflow: auto;
-            background-image: url('../../assets/asset_web/bg3.png');
+            border: none;
+            outline: none;
+            box-sizing: border-box;
+            font-family: "Poppins";
+        }
+        body {
+            display: flex;
+        }
+        .sidebar{
+            position: sticky;
+            top: 0;
+            left: 0;
+            bottom: 0;
+            width: 250px;
+            height: 100vh;
+            padding: 0 1.7rem;
+            color: #fff;
+            overflow: hidden;
+            transition: all 0.5s linear;
+            background-image: url("../../assets/asset_web/bg3.png");
         }
 
-        li a {
-            display: block;
-            color: #F5F7F8;
-            padding: 16px 16px;
-            text-decoration: none;
+        .menu{
+            height: 88%;
+            position: relative;
+            list-style: none;
+            padding-top: 35px;
             text-align: center;
-            font-size: 15px;
+            padding-left: 0px;
         }
         .logo{
-            padding: 30px 20px;
-            font-size: 20px;
+            font-size: 18px;
+            height: 80px;
+            padding: 20px;
+            text-align: center;
+            overflow: initial;
+        
         }
-        .about{
-            margin-top: 140px;
+
+        .menu li{
+            padding: 15px;
+            text-align: center;
         }
+
+        .menu li a{
+            color: white;
+            text-decoration: none;
+            gap: 10px;
+            display: flex;
+            align-items: center;  
+        }
+
+        .menu li a:hover{
+            color:antiquewhite;
+        }
+
+        .menu span{
+            overflow: hidden;
+            
+        }
+        .logout{
+            position: absolute;
+            bottom: 20px;
+            width: 100%;
+        }
+
+        
         .welcome{
-            padding-left : 55%;
+            text-align: right;
+            padding-right: 15px;
             padding-top : 15px;
 
-        }
-        .user{
-            width: 100%;
-            background-color: white;
-            position: fixed;
-            z-index: 1;
-        }
-        .container{
-            width:75%;
-            margin-top: 5%;
-            margin-left: 20px;
-            margin-right: 20px;
-            position: absolute;
-            background: #c3c1c1;
-            overflow:auto;
         }
         .check{
             width:100%;
@@ -83,26 +110,39 @@
     </style>
 </head>
 <body>
-    <div class="vernav">
-        <ul>
-            <li><a class="logo" href="#">BrandName</a></li>
-            <li><a class="active" href="dashboard.php"><img src="../../assets/asset_web/dash.png"> Dashboard</a></li>
-            <li><a href="#news"><img src="../../assets/asset_web/class.png"> Class</a></li>
-            <li><a href="#contact"><img src="../../assets/asset_web/Paper.png"> Report</a></li>
-            <li><a href="#contact"><img src="../../assets/asset_web/date.png"> Schedule</a></li>
-            <li><a class="about" href="#about"><img class="out" src="../../assets/asset_web/out.png"> Log Out</a></li>
+<div class="sidebar">
+        <div class="logo">Dana English Course</div>
+        <ul class="menu">
+            <li>
+                <a href="#"><i class="fa fa-tachometer-alt"></i><span> Dashboard</span></a>
+            </li>
+            <li>
+                <a href="#"><i class="fa fa-graduation-cap"></i><span> Class</span></a>
+            </li>
+            <li>
+                <a href="#"><i class="fa fa-book"></i><span> Report</span></a>
+            </li>
+            <li>
+                <a href="#"><i class="fa fa-archive"></i><span> Buy Package</span></a>
+            </li>
+            <li>
+                <a href="#"><i class="fa fa-calendar"></i><span> Schedule</span></a>
+            </li>
+            <li class="logout">
+                <a href="#"><i class="fa fa-sign-out"></i><span> Log out</span></a>
+            </li>
         </ul>
     </div>
 
-    <div style="margin-left:20%;padding:0px 16px;">
-        <div class="user">
-        <h4 class="welcome">WELCOME USER</h4>
-    </div>
+        
 
     <div class="container">
         <?php 
             while($row = mysqli_fetch_assoc($result)){ 
         ?>
+        <div class="user">
+             <h4 class="welcome">WELCOME USER</h4>
+        </div>
         <div class="row">
             <div class="col-md-6">
                 <div class="student-info">
--- php/Guru_Page/detailNilaiSiswa.php
+++ php/Guru_Page/detailNilaiSiswa.php
@@ -14,169 +14,227 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 
     <style>
-        body {
-            margin: 0;
-            font-family: 'Montserrat', sans-serif;
-        }
-        ul {
-            list-style-type: none;
+        @import url("https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap");
+        *{
             margin: 0;
             padding: 0;
-            width: 20%;
-            background-color: #f1f1f1;
-            position: fixed;
-            height: 100%;
-            overflow: auto;
-            background-image: url('../../assets/asset_web/bg3.png');
+            border: none;
+            outline: none;
+            box-sizing: border-box;
+            font-family: "Poppins";
+        }
+        body {
+            display: flex;
+        }
+        .sidebar{
+            position: sticky;
+            top: 0;
+            left: 0;
+            bottom: 0;
+            width: 250px;
+            height: 100vh;
+            padding: 0 1.7rem;
+            color: #fff;
+            overflow: hidden;
+            transition: all 0.5s linear;
+            background-image: url("../../assets/asset_web/bg3.png");
         }
 
-        li a {
-            display: block;
-            color: #F5F7F8;
-            padding: 16px 16px;
-            text-decoration: none;
+        .menu{
+            height: 88%;
+            position: relative;
+            list-style: none;
+            padding-top: 35px;
             text-align: center;
-            font-size: 15px;
+            padding-left: 0px;
+            /* justify-content: center; */
         }
         .logo{
-            padding: 30px 20px;
-            font-size: 20px;
+            font-size: 18px;
+            height: 80px;
+            padding: 20px;
+            text-align: center;
+            overflow: initial;
         }
-        .about{
-            margin-top: 140px;
+
+        .menu li{
+            padding: 15px;
+            text-align: center;
+            /* justify-content: center; */
         }
+
+        .menu li a{
+            color: white;
+            text-decoration: none;
+            gap: 10px;
+            display: flex;
+            align-items: center;  
+        }
+
+        .menu li a:hover{
+            color:antiquewhite;
+        }
+
+        .menu span{
+            overflow: hidden;
+            
+        }
+        .logout{
+            position: absolute;
+            bottom: 20px;
+            width: 100%;
+        }
+
+        
         .welcome{
-            padding-left : 55%;
+            text-align: right;
+            padding-right: 15px;
             padding-top : 15px;
 
         }
-        .user{
-            width: 100%;
-            background-color: white;
-            position: fixed;
-            z-index: 1;
-        }
         .student-info{
-            width:400px;
-            padding:20px;
-            margin-top: 5%;
-            margin-left: 30px;
-            margin-right: 20px;
-            position: absolute;
-            background: #f3f3f3;
+            background: #F3F3F3;
+            width: 40%;
+            border-radius: 10px;
+            padding: 20px;
+        }
+        .student-info table tr td{
+            text-align:left;
         }
         .button-container{
-            margin-top: 13%;
-            width:75%;
-            position: absolute;
-            display: inline;
+            width:100%;
         }
         .button-container .btn{
-            float: right;
+            /* float: right; */
             color: white;
-            margin: 20px 10px 0;
-            width: 140px;
+            margin: 20px 10px 20px;
         }
         .table{
-            width:75%;
-            margin-top:20%;
-            position:absolute;
-            overflow:auto;
-            table-layout: fixed;
+ 
         }
         td{
             width:33%;
             text-align: center;
             vertical-align:middle;
         }
-        .btnback{
-            width:75%;
-            margin-top:45%;
-            position:absolute;
-        }
-        a{
+        .btnback a button{
+            background: #AEAEAE;
+            padding:10px;
+            border-radius:10px;
+            /* margin: 20px 10px 20px; */
 
-            float: right;
         }
     </style>
 </head>
 <body>
-    <div class="vernav">
-            <ul>
-                <li><a class="logo" href="#">BrandName</a></li>
-                <li><a class="active" href="dashboard.php"><img src="../../assets/asset_web/dash.png"> Dashboard</a></li>
-                <li><a href="#news"><img src="../../assets/asset_web/class.png"> Class</a></li>
-                <li><a href="#contact"><img src="../../assets/asset_web/Paper.png"> Report</a></li>
-                <li><a href="#contact"><img src="../../assets/asset_web/date.png"> Schedule</a></li>
-                <li><a class="about" href="#about"><img class="out" src="../../assets/asset_web/out.png"> Log Out</a></li>
-            </ul>
+<div class="sidebar">
+        <div class="logo">Dana English Course</div>
+        <ul class="menu">
+            <li>
+                <a href="#"><i class="fa fa-tachometer-alt"></i><span> Dashboard</span></a>
+            </li>
+            <li>
+                <a href="#"><i class="fa fa-graduation-cap"></i><span> Class</span></a>
+            </li>
+            <li>
+                <a href="#"><i class="fa fa-book"></i><span> Report</span></a>
+            </li>
+            <li>
+                <a href="#"><i class="fa fa-archive"></i><span> Buy Package</span></a>
+            </li>
+            <li>
+                <a href="#"><i class="fa fa-calendar"></i><span> Schedule</span></a>
+            </li>
+            <li class="logout">
+                <a href="#"><i class="fa fa-sign-out"></i><span> Log out</span></a>
+            </li>
+        </ul>
     </div>
 
-    <div style="margin-left:20%;padding:0px 16px;">
+        
+
+    <div class="container">
         <div class="user">
-        <h4 class="welcome">WELCOME USER</h4>
+             <h4 class="welcome">WELCOME USER</h4>
+        </div>
+        <div class="student-info">
+            <table>
+                <tr>
+                    <td>Name</td>
+                    <td >:<?=" "?><?=$result2['Nama_Siswa']; ?></td>
+                </tr>
+                <tr>
+                    <td>Class</td>
+                    <td >:<?php  
+                            $id = $result2['Id_Siswa'];
+                            $sql2 = "SELECT * FROM class WHERE Id_Siswa = $id";
+                            $res = mysqli_query($conn,$sql2);
+                            $res2 = mysqli_fetch_assoc($res);
+                            echo " ";
+                            echo $res2['Nama_Class'];
+                        ?></td>
+                </tr>
+                <tr>
+                    <td>No Pendaftaran</td>
+                    <td >: <?=$id; ?></td>
+                </tr>
+            </table>
+        </div>
+        
+        <div class="button-container d-flex justify-content-end">
+            <button type="button" class="btn" style="background:#5ab0ff;">Save</button>
+            <button type="button" class="btn" style="background:#002d66;">Edit</button>
+        </div>
+
+        <table class="table table-bordered">
+            <thead class="table-primary">
+                <tr>
+                    <th>Tugas</th>
+                    <th>Nilai</th>
+                    <th>Keterangan</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td rowspan="3">Tugas</td>
+                    <td>tes</td>
+                    <td>tes</td>
+                </tr>
+                <tr>
+                    <td>tes</td>
+                    <td>tes</td>
+                </tr>
+                <tr>
+                    <td>tes</td>
+                    <td>tes</td>
+                </tr>
+                <tr>
+                    <td rowspan="3">TES</td>
+                    <td>tes</td>
+                    <td>tes</td>
+                </tr>
+                <tr>
+                    <td>tes</td>
+                    <td>tes</td>
+                </tr>
+                <tr>
+                    <td>tes</td>
+                    <td>tes</td>
+                </tr>
+            </tbody>
+        </table>
+
+        <div class="btnback">
+            <a href="daftarSiswa.php"><button type="button">< Kembali</button></a>
+        </div>
     </div>
 
-    <div class="student-info">
-        <p>Nama: <?= $result2['Nama_Siswa']; ?></p>
-        <p>Class: <?php  
-                        $id = $result2['Id_Siswa'];
-                        $sql2 = "SELECT * FROM class WHERE Id_Siswa = $id";
-                        $res = mysqli_query($conn,$sql2);
-                        $res2 = mysqli_fetch_assoc($res);
-                        echo $res2['Nama_Class'];
-                    ?></p>
-        <p>No Pendaftaran: <?= $id; ?></p>
-    </div>
+    
 
-    <div class="button-container">
-        <button type="button" class="btn" style="background:#5ab0ff;">Save</button>
-        <button type="button" class="btn" style="background:#002d66;">Edit</button>
-    </div>
-
-    <table class="table table-bordered">
-        <thead class="table-primary">
-            <tr>
-                <th>Tugas</th>
-                <th>Nilai</th>
-                <th>Keterangan</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <td rowspan="3">Tugas</td>
-                <td>tes</td>
-                <td>tes</td>
-            </tr>
-            <tr>
-                <td>tes</td>
-                <td>tes</td>
-            </tr>
-            <tr>
-                <td>tes</td>
-                <td>tes</td>
-            </tr>
-            <tr>
-                <td rowspan="3">TES</td>
-                <td>tes</td>
-                <td>tes</td>
-            </tr>
-            <tr>
-                <td>tes</td>
-                <td>tes</td>
-            </tr>
-            <tr>
-                <td>tes</td>
-                <td>tes</td>
-            </tr>
-        </tbody>
-    </table>
-
-    <div class="btnback">
-        <a href="daftarSiswa.php"><button type="button">< Kembali</button></a>
-    </div>
+    
 </body>
 </html>
