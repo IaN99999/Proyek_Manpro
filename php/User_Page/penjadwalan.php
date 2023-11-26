e64967290b204b9f7b48e9423aac6accd040a3cb
agathacelia <90375290+agathacelia@users.noreply.github.com>
2023-11-27 02:27:53 +0700

    Penjadwalan

---
+++ php/User_Page/penjadwalan.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Course Schedule</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/boostrap@5.3.0/dist/js/bootstrap.bundle.min.js></script>
+</head>
+<body>
+    <?php include 'navbar_footer/navbar.php'; ?>
+
+    <h1>Jadwal Kursus Bahasa Inggris</h1>
+
+    <div class="jadwal-container">
+        <?php
+        $servername = "localhost";
+        $username = "username";
+        $password = "password";
+        $dbname = "manpro";
+
+        $conn = new mysqli($servername, $username, $password, $dbname);
+
+        if ($conn->connect_error) {
+            die("Koneksi gagal: " . $conn->connect_error);
+        }
+
+        $sql = "SELECT jadwal.Id_Jadwal, class.Nama_Class, jadwal.Tanggal_Jadwal FROM jadwal INNER JOIN class ON jadwal.Id_Class = class.Id_Class";
+        $result = $conn->query($sql);
+
+        if ($result->num_rows > 0) {
+            echo "<table border='1'>
+            <tr>
+                <th>ID Jadwal</th>
+                <th>Nama Kelas</th>
+                <th>Tanggal</th>
+            </tr>";
+
+            while($row = $result->fetch_assoc()) {
+                echo "<tr>
+                    <td>" . $row["Id_Jadwal"]. "</td>
+                    <td>" . $row["Nama_Class"]. "</td>
+                    <td>" . $row["Tanggal_Jadwal"]. "</td>
+                </tr>";
+            }
+
+            echo "</table>";
+        } else {
+            echo "Tidak ada data jadwal.";
+        }
+
+        $conn->close();
+        ?>
+    </div>
+
+    <?php include 'navbar_footer/footer.php'; ?>
+
+</body>
+</html>
