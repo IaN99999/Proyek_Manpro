152d4912b80043b237b1d8db8ffedce5ea022656
agathacelia <90375290+agathacelia@users.noreply.github.com>
2023-12-08 21:32:03 +0700

    update daftar harga

--- php/Home_Page/daftarHarga.php
+++ php/Home_Page/daftarHarga.php
@@ -1,54 +1,47 @@
+<?php
+include '../connection.php';
+$sql = "SELECT harga.session, harga.Price, class.Nama_Class FROM harga INNER JOIN class on harga.Id_Class = class.Id_Class";
+$result = mysqli_query($conn, $sql);
+?>
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Daftar Harga Kursus</title>
+    <title>Price List</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
 </head>
+
 <body>
     <div class="container">
-        <h1>Daftar Harga Kursus Bahasa Inggris</h1>
-        <div class="row">
+        <h1>Price List</h1>
+        <div class="row py-5">
             <?php
-            
-            $host = "localhost"; 
-            $username = "username";
-            $password = "password"; 
-            $database = "hargakursus"; 
-            
-            
-            $conn = mysqli_connect($host, $username, $password, $database);
-            
-            
-            if (!$conn) {
-                die("Koneksi ke database gagal: " . mysqli_connect_error());
-            }
 
-            
-            $sql = "SELECT * FROM hargakursus";
-            $result = mysqli_query($conn, $sql);
 
             if (mysqli_num_rows($result) > 0) {
                 while ($row = mysqli_fetch_assoc($result)) {
             ?>
-                <div class="col-lg-4 col-md-6 mb-4">
-                    <div class="card">
-                        <div class="card-body">
-                            <h5 class="card-title"><?php echo $row['id_kelas']; ?></h5>
-                            <p class="card-text">Sesi per Bulan: <?php echo $row['sesi_perBulan']; ?></p>
-                            <p class="card-text">Harga: <?php echo $row['harga']; ?></p>
+                    <div class="col-lg-4 col-md-6 mb-4">
+                        <div class="card">
+                            <div class="card-body">
+                                <p class="card-text bg-secondary px-2 py-1 text-light rounded">Class Type: <?php echo $row['Nama_Class']; ?></p>
+                                <p class="card-text">Session per month: <?php echo $row['session']; ?></p>
+                                <p class="card-text">Price: <button class="btn btn-sm btn-success">Rp.<?php echo number_format($row['Price']); ?></button> </p>
+                            </div>
                         </div>
                     </div>
-                </div>
             <?php
                 }
             } else {
-                echo "Belum ada data harga kursus yang tersedia.";
+                echo "No price available.";
             }
             ?>
         </div>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
+
 </html>
